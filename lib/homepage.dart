de63ec4b7c9923113e0ef2b1dc809492bf949e3c
Supakorn Yookack <foczza@gmail.com>
2022-03-12 14:20:37 +0700

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> food = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Food'),
+      ),
+      body:
+          Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:
+            Expanded(
+                child: ListView.builder(
+                    itemCount: food.length,
+                    itemBuilder: (BuildContext context,int index){
+                      return ListTile(
+                          leading: Image.network(food[index].image),
+                          trailing: Text( '${food[index].price} บาท',
+                            style: TextStyle(
+                                color: Colors.green,fontSize: 15),),
+                          title:Text(food[index].name)
+                      );
+                    }
+                ),),
+          ],
+        ),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _handleClickButton() async {
+    final url = Uri.parse('https://cpsu-test-api.herokuapp.com/foods');
+    var result = await http.get(url);
+    var json = jsonDecode(result.body);
+    food = json['data'];
+    print(food);
+  }
+}
